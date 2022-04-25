cc8bc2c67673c556c695d5ff3c5d951c3bd3b21e
황정준 <hsk2454@naver.com>
2022-04-25 19:17:00 +0900

    Feat : Spring Boot 기본 세팅

---
+++ src/main/java/com/chwimi/bobchoo/BobchooApplication.java
@@ -0,0 +1,13 @@
+package com.chwimi.bobchoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobchooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobchooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chwimi/bobchoo/BobchooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chwimi.bobchoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobchooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
