6ebc1da34d34ae2c843ec66e5172fe51e535e5bd
황정준 <hsk2454@naver.com>
2022-05-10 16:56:25 +0900

    Chore : 모든 Controller에 Swagger 코드 추가

--- src/main/java/com/chwimi/bobchoo/domain/info/controller/InfoController.java
+++ src/main/java/com/chwimi/bobchoo/domain/info/controller/InfoController.java
@@ -2,9 +2,12 @@ package com.chwimi.bobchoo.domain.info.controller;
 
 import com.chwimi.bobchoo.domain.info.dto.CopyrightResDto;
 import com.chwimi.bobchoo.domain.info.service.InfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "Info API", tags = {"Info"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/info")
@@ -13,6 +16,7 @@ public class InfoController {
     private final InfoService infoService;
 
     @GetMapping("/copyrights")
+    @ApiOperation(value = "음식 사진 저작권 정보 반환", notes = "인터넷에서 가져온 사진만 저작권 표시")
     public CopyrightResDto copyright() {
         return infoService.getCopyright();
     }
--- src/main/java/com/chwimi/bobchoo/domain/survey/controller/SurveyController.java
+++ src/main/java/com/chwimi/bobchoo/domain/survey/controller/SurveyController.java
@@ -7,11 +7,14 @@ import com.chwimi.bobchoo.domain.survey.dto.SurveyResultReqDto;
 import com.chwimi.bobchoo.domain.survey.service.SurveyService;
 import com.chwimi.bobchoo.global.common.BaseResponse;
 import com.chwimi.bobchoo.global.common.StatusEnum;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "음식 설문 추천 API", tags = {"Surveys"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/surveys")
@@ -20,16 +23,19 @@ public class SurveyController {
     private final SurveyService surveyService;
 
     @GetMapping
+    @ApiOperation(value = "설문 목록 반환")
     public SurveyResDto surveys() {
         return surveyService.getSurveys();
     }
 
     @PostMapping("/results")
+    @ApiOperation(value = "설문 추천 결과 반환")
     public FoodResDto result(@RequestBody SurveyResultReqDto resultReqDto) {
         return surveyService.surveyResult(resultReqDto);
     }
 
     @PostMapping("/results/list")
+    @ApiOperation(value = "설문 추천목록 결과 반환")
     public List<FoodResDto> resultList(@RequestBody SurveyResultReqDto resultReqDto) {
         return surveyService.surveyResultList(resultReqDto);
     }
