bca60cee81e05a4b675fb9635aab62779102bbd9
황정준 <hsk2454@naver.com>
2022-05-18 17:08:33 +0900

    Feat : 음식 정보 반환 api 추가

--- src/main/java/com/chwimi/bobchoo/domain/info/controller/InfoController.java
+++ src/main/java/com/chwimi/bobchoo/domain/info/controller/InfoController.java
@@ -2,6 +2,7 @@ package com.chwimi.bobchoo.domain.info.controller;
 
 import com.chwimi.bobchoo.domain.info.dto.CopyrightResDto;
 import com.chwimi.bobchoo.domain.info.service.InfoService;
+import com.chwimi.bobchoo.global.dto.FoodResDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -20,4 +21,12 @@ public class InfoController {
     public CopyrightResDto copyright() {
         return infoService.getCopyright();
     }
+
+    @GetMapping("/food/{name}")
+    @ApiOperation(value = "음식 정보 반환", notes = "음식 이름을 받아서 음식 사진 반환")
+    public FoodResDto food(@PathVariable("name") String foodName) {
+        return infoService.getFoodInfo(foodName);
+    }
+
+
 }
--- src/main/java/com/chwimi/bobchoo/domain/info/service/InfoService.java
+++ src/main/java/com/chwimi/bobchoo/domain/info/service/InfoService.java
@@ -1,7 +1,10 @@
 package com.chwimi.bobchoo.domain.info.service;
 
 import com.chwimi.bobchoo.domain.info.dto.CopyrightResDto;
+import com.chwimi.bobchoo.global.dto.FoodResDto;
 
 public interface InfoService {
     CopyrightResDto getCopyright();
+
+    FoodResDto getFoodInfo(String foodName);
 }
